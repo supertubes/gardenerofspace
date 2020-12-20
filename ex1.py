b0bd5f2fa425daff13313ad60b02de8e09365cba
behrend martens <behrendmartens@gmail.com>
2020-12-20 13:39:45 -0800

    first

---
+++ ex1.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+print("I can say many i love you wife and daughter")
+
+
+
+print ("Hens", 25 + 30 /6)
+print ("Chickens", 100 - 50 % 2)
+
+cars = 100
+dogs = 50
+machines = 3
+
+
+
+print ("cars + dogs")
---
+++ scriptsplay.py
@@ -0,0 +1,12 @@
+string1 = "Linux"
+string2 = "Hint"
+joined_string = string1 + string2
+print(joined_string)
+
+
+# Initialize the list
+weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"]
+print("Seven Weekdays are:\n")
+# Iterate the list using for loop
+for day in range(len(weekdays)):
+    print(weekdays[day])
